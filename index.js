f5fe3dfa036404f463fd3a399c2ea82c6e1459bc
Annika Kritzinger <121497951+annikakritzinger03@users.noreply.github.com>
2024-05-22 19:18:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+    WPR381 Assignment 1 Group V12:
+        - Annika Kritzinger (577322)
+        - Nathan Evans (577806)
+        - Thabang Mahlangu (576277)
+        - Kyle Tarran (576509) (Did Not Contribute)
+*/
+
